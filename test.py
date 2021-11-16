4b504c2a44832952bb65c766b493bcb5d6138d17
Karan V <karan.e0119039@sret.edu.in>
2021-11-16 10:30:49 +0530

    fixed error

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
-import AES_hybrid
+from AES_hybrid import *
 import time
 import base64
 
 with open("googlefooter.png", "rb") as img_file:
     my_string = base64.b64encode(img_file.read())
-   
+
 start_time = time.time()
-encry=aes_2.encrypt('ahdfsujeytsbsdfawskdfhsdgfereijd', my_string)
+enc = AES()
+
+encry = enc.encrypt_cbc('ahdfsujeytsbsdfawskdfhsdgfereijd', my_string)
 print("time for encryption --- %s seconds ---" % (time.time() - start_time))
 with open("encry_test_Img.enc", "wb") as img_file:
     img_file.write(encry)
 
 start_time2 = time.time()
-decry=aes_2.decrypt('ahdfsujeytsbsdfawskdfhsdgfereijd',encry)
+decry = enc.decrypt_cbc('ahdfsujeytsbsdfawskdfhsdgfereijd', encry)
 print("time for decryption --- %s seconds ---" % (time.time() - start_time2))
 with open("out_test_Img.jpeg", "wb") as img_file:
     img_file.write(base64.b64decode(decry))
